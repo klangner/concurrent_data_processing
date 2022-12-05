798b568a60c8ae544cb15a63e142289567c27d54
klangner <klangner@gmail.com>
2022-12-05 21:39:14 +0100

    Supervisor

--- jobber/lib/jobber.ex
+++ jobber/lib/jobber.ex
@@ -1,18 +1,12 @@
 defmodule Jobber do
+  alias Jobber.{JobRunner, JobSupervisor}
   @moduledoc """
   Documentation for `Jobber`.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Jobber.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def start_job(args) do
+    DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
   end
 end
