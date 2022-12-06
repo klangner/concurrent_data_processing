46186ffceb6f86bf0e9a4c9b5ab62ed39e55901a
klangner <klangner@gmail.com>
2022-12-06 09:25:56 +0100

    jobber done

--- jobber/lib/jobber.ex
+++ jobber/lib/jobber.ex
@@ -7,6 +7,18 @@ defmodule Jobber do
   @doc """
   """
   def start_job(args) do
-    DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
+    if (Enum.count(running_imports()) < 5) do
+      DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
+    else
+      {:error, :import_quota_reached}
+    end
+  end
+
+  def running_imports() do
+    match_all = {:"$1", :"$2", :"$3"}
+    guards = [{:"==", :"$3", "import"}]
+    map_result = [%{id: :"$1", pid: :"$2", type: :"$3"}]
+
+    Registry.select(Jobber.JobRegistry, [{match_all, guards, map_result}])
   end
 end
