350a8772a5fa2d5656338ad68cdf16da4b748572
klangner <klangner@gmail.com>
2022-12-09 23:46:35 +0100

    flow stream

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -8,6 +8,23 @@ alias NimbleCSV.RFC4180, as: CSV
     Application.app_dir(:airports, "/priv/airports.csv")
   end
 
+  def open_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
+  end
+
   def open_airports_naive() do
     airports_csv()
     |> File.read!()
@@ -36,7 +53,6 @@ alias NimbleCSV.RFC4180, as: CSV
       }
     end)
     |> Stream.reject(&(&1.type == "closed"))
-    # |> Stream.run()
     |> Enum.to_list()
   end
 
